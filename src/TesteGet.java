f58ee60d49268ad2d6aa3af39aec0881c59df87d
Joao Trindade <trindade.joao@gmail.com>
2011-11-16 17:37:38 +0000

    Added text

--- src/TesteGet.java
+++ src/TesteGet.java
@@ -2,31 +2,31 @@ public class TesteGet{
 
 	/* Use jdb to run line by line*/
 	public static void main(String args[]){
-		System.out.println(ControlFunctions.getLightAll());
-		System.out.println(ControlFunctions.getLightLedTV());
-		System.out.println(ControlFunctions.getLightBoard());
-		System.out.println(ControlFunctions.getLightMiddle1());
-		System.out.println(ControlFunctions.getLightMiddle2());
-		System.out.println(ControlFunctions.getLightTv());
-		System.out.println(ControlFunctions.getSocketsAll());
-		System.out.println(ControlFunctions.getBlindsBoard());
-		System.out.println(ControlFunctions.getBlindsMidle());
-		System.out.println(ControlFunctions.getBlindsTv());
-		System.out.println(ControlFunctions.getBlindsAll());
-		System.out.println(ControlFunctions.getDoor());
-		System.out.println(ControlFunctions.getOutsideTemperature());
-		System.out.println(ControlFunctions.getOutsideWind());
-		System.out.println(ControlFunctions.getOutsideWeather());
-		System.out.println(ControlFunctions.getInsideCo2());
-		System.out.println(ControlFunctions.getInsideHumidity());
-		System.out.println(ControlFunctions.getInsideTemperature());
-		System.out.println(ControlFunctions.getInsideLuminosity());
+		System.out.println("Light: " + ControlFunctions.getLightAll());
+		System.out.println("Light LED TV: " + ControlFunctions.getLightLedTV());
+		System.out.println("Light Light board: " + ControlFunctions.getLightBoard());
+		System.out.println("Light Middle: " + ControlFunctions.getLightMiddle1());
+		System.out.println("Light Middle: 2" + ControlFunctions.getLightMiddle2());
+		System.out.println("Light Tv: " + ControlFunctions.getLightTv());
+		System.out.println("Sockets All: " + ControlFunctions.getSocketsAll());
+		System.out.println("Get blinds board: " + ControlFunctions.getBlindsBoard());
+		System.out.println("Get blinds middle: " + ControlFunctions.getBlindsMidle());
+		System.out.println("Get blinds middle: " + ControlFunctions.getBlindsTv());
+		System.out.println("Get blinds all: "+ ControlFunctions.getBlindsAll());
+		System.out.println("Get door: " + ControlFunctions.getDoor());
+		System.out.println("Get outside temp: " + ControlFunctions.getOutsideTemperature());
+		System.out.println("Get outside Wind: " + ControlFunctions.getOutsideWind());
+		System.out.println("Get outside weather: " + ControlFunctions.getOutsideWeather());
+		System.out.println("Get inside Co2: " + ControlFunctions.getInsideCo2());
+		System.out.println("Get inside humidity: " + ControlFunctions.getInsideHumidity());
+		System.out.println("Get inside temperature: " + ControlFunctions.getInsideTemperature());
+		System.out.println("Get inside luminosity: " + ControlFunctions.getInsideLuminosity());
 		
-		System.out.println(ControlFunctions.getAirConditionerFanSpeed());
-		System.out.println(ControlFunctions.getAirConditionerOnOff());
-		System.out.println(ControlFunctions.getAirConditionerSwing());
-		System.out.println(ControlFunctions.getAirConditionerMode());
-		System.out.println(ControlFunctions.getAirConditionerTemperature());
+		System.out.println("Get AC Fan speed: " + ControlFunctions.getAirConditionerFanSpeed());
+		System.out.println("Get AC on/off: " + ControlFunctions.getAirConditionerOnOff());
+		System.out.println("Get AC swing: " + ControlFunctions.getAirConditionerSwing());
+		System.out.println("Get AC Mode: " + ControlFunctions.getAirConditionerMode());
+		System.out.println("Get AC Temp: " + ControlFunctions.getAirConditionerTemperature());
 		
 	}
 }
