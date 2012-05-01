85d51b7e583a95b0860dfdcd6ba78ff9dcaac842
Joao Trindade <trindade.joao@gmail.com>
2012-05-01 22:57:18 +0100

    Think I solved the JSON problem

--- src/GetOperators.java
+++ src/GetOperators.java
@@ -12,47 +12,45 @@
 import com.google.gson.*;
 import java.util.*;
 
-
 public class GetOperators{
 	/** Get info about the "device" in "division"*/	
 	public static Map<String,String> getInfo(int division,int device){
 			String url = URLGet.urlGetGenerator(division); //generate URL
 			String json = URLGet.urlGet(url);				//get JSON file from URL
-	
+
 			Map<String,String> data = readDevice(json,device);				//get info from JSON
 			if(data.size() == 0){
 				System.out.println("Read error");
 			}	
 			return data;
 	}
-	
-	
+
+
 	/**Read the device id from the JSON string
 	 * @return Map <PropertyName,PropertyValue>*/
 	public static Map<String,String> readDevice(String json,Integer id){
 		Map<String,String> data = new HashMap<String,String>();
 		JsonElement jsonElement = new JsonParser().parse(json); //[{\"id\":\"114\"}]
-        
+
 		JsonArray deviceObjectArray = jsonElement.getAsJsonArray(); //[{\"id\":\"114\"}] 
 
 		for(JsonElement object: deviceObjectArray){
-				JsonObject jso = object.getAsJsonObject();	
-				JsonObject deviceProperties = jso.getAsJsonObject("DeviceProperties");
+				JsonObject jso = object.getAsJsonObject();
+				JsonArray deviceProperties = jso.getAsJsonArray("DeviceProperty");
 
-				Set<Map.Entry<String,JsonElement>> set = deviceProperties.entrySet();  //device properties SET
-				
-				for(Map.Entry<String,JsonElement> entry : set){
-					JsonElement prop = entry.getValue();
-					JsonObject jsoProp = prop.getAsJsonObject();
-					String device_id = jsoProp.get("device_id").toString().replace("\"",""); //delete "" element;
-			
-					if(device_id.equals(id.toString())){
-						String prop_value = jsoProp.get("value").toString().replace("\"","");
-						String prop_name = jsoProp.get("name").toString().replace("\"","");
-						data.put(prop_name,prop_value);
-					}
-				}
-				
+                for (int i = 0; i < deviceProperties.size(); i++) {
+
+                    JsonElement prop = deviceProperties.get(i);
+
+                    JsonObject jsoProp = prop.getAsJsonObject();
+                    String device_id = jsoProp.get("device_id").toString().replace("\"",""); //delete "" element;
+
+                    if(device_id.equals(id.toString())){
+                        String prop_value = jsoProp.get("value").toString().replace("\"","");
+                        String prop_name = jsoProp.get("name").toString().replace("\"","");
+                        data.put(prop_name,prop_value);
+                    }
+                }
 			}
 			return data;
 	}
--- src/TesteGet.java
+++ src/TesteGet.java
@@ -3,30 +3,30 @@ public class TesteGet{
 	/* Use jdb to run line by line*/
 	public static void main(String args[]){
 		System.out.println("Light: " + ControlFunctions.getLightAll());
-		System.out.println("Light LED TV: " + ControlFunctions.getLightLedTV());
-		System.out.println("Light Light board: " + ControlFunctions.getLightBoard());
-		System.out.println("Light Middle: " + ControlFunctions.getLightMiddle1());
-		System.out.println("Light Middle: 2" + ControlFunctions.getLightMiddle2());
-		System.out.println("Light Tv: " + ControlFunctions.getLightTv());
-		System.out.println("Sockets All: " + ControlFunctions.getSocketsAll());
-		System.out.println("Get blinds board: " + ControlFunctions.getBlindsBoard());
-		System.out.println("Get blinds middle: " + ControlFunctions.getBlindsMidle());
-		System.out.println("Get blinds middle: " + ControlFunctions.getBlindsTv());
-		System.out.println("Get blinds all: "+ ControlFunctions.getBlindsAll());
-		System.out.println("Get door: " + ControlFunctions.getDoor());
-		System.out.println("Get outside temp: " + ControlFunctions.getOutsideTemperature());
-		System.out.println("Get outside Wind: " + ControlFunctions.getOutsideWind());
-		System.out.println("Get outside weather: " + ControlFunctions.getOutsideWeather());
-		System.out.println("Get inside Co2: " + ControlFunctions.getInsideCo2());
-		System.out.println("Get inside humidity: " + ControlFunctions.getInsideHumidity());
-		System.out.println("Get inside temperature: " + ControlFunctions.getInsideTemperature());
-		System.out.println("Get inside luminosity: " + ControlFunctions.getInsideLuminosity());
-		
-		System.out.println("Get AC Fan speed: " + ControlFunctions.getAirConditionerFanSpeed());
-		System.out.println("Get AC on/off: " + ControlFunctions.getAirConditionerOnOff());
-		System.out.println("Get AC swing: " + ControlFunctions.getAirConditionerSwing());
-		System.out.println("Get AC Mode: " + ControlFunctions.getAirConditionerMode());
-		System.out.println("Get AC Temp: " + ControlFunctions.getAirConditionerTemperature());
+//        System.out.println("Light LED TV: " + ControlFunctions.getLightLedTV());
+//        System.out.println("Light Light board: " + ControlFunctions.getLightBoard());
+//        System.out.println("Light Middle: " + ControlFunctions.getLightMiddle1());
+//        System.out.println("Light Middle: 2" + ControlFunctions.getLightMiddle2());
+//        System.out.println("Light Tv: " + ControlFunctions.getLightTv());
+//        System.out.println("Sockets All: " + ControlFunctions.getSocketsAll());
+//        System.out.println("Get blinds board: " + ControlFunctions.getBlindsBoard());
+//        System.out.println("Get blinds middle: " + ControlFunctions.getBlindsMidle());
+//        System.out.println("Get blinds middle: " + ControlFunctions.getBlindsTv());
+//        System.out.println("Get blinds all: "+ ControlFunctions.getBlindsAll());
+//        System.out.println("Get door: " + ControlFunctions.getDoor());
+//        System.out.println("Get outside temp: " + ControlFunctions.getOutsideTemperature());
+//        System.out.println("Get outside Wind: " + ControlFunctions.getOutsideWind());
+//        System.out.println("Get outside weather: " + ControlFunctions.getOutsideWeather());
+//        System.out.println("Get inside Co2: " + ControlFunctions.getInsideCo2());
+//        System.out.println("Get inside humidity: " + ControlFunctions.getInsideHumidity());
+//        System.out.println("Get inside temperature: " + ControlFunctions.getInsideTemperature());
+//        System.out.println("Get inside luminosity: " + ControlFunctions.getInsideLuminosity());
+//        
+//        System.out.println("Get AC Fan speed: " + ControlFunctions.getAirConditionerFanSpeed());
+//        System.out.println("Get AC on/off: " + ControlFunctions.getAirConditionerOnOff());
+//        System.out.println("Get AC swing: " + ControlFunctions.getAirConditionerSwing());
+//        System.out.println("Get AC Mode: " + ControlFunctions.getAirConditionerMode());
+//        System.out.println("Get AC Temp: " + ControlFunctions.getAirConditionerTemperature());
 		
 	}
 }
